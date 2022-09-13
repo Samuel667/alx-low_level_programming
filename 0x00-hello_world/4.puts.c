234c831a9ed1d581673fbc35cb8cde49e39f572d
root <samuel i>
2022-09-12 20:49:53 -0700

    puts.c

--- 0x00-hello_world/4.puts.c
+++ 0x00-hello_world/4.puts.c
@@ -1,1 +1,2 @@
 #include  < stdio.h >;                  int main(void){programming is like building a multilingual puzzle\n                                          return(0)}
+#include  < stdio.h >;                  int main(void){programming is like building a multilingual puzzle\n                                          return(0)}
